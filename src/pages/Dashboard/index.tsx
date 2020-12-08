4d5a324fab737118ad63546a0a31012b86067670
Julio Cesar <jcesar_olv@outlook.com>
2020-12-08 16:27:16 -0300

    stylizing the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <Title>Explore repositórios no Github</Title>
+    <>
+      <img src={logoImg} alt='Github Explorer' />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder='Digite o nome do repositório'/>
+        <button type='submit' >Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48095783?s=460&v=4"
+            alt="Julio Cesar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJs & React Native forms! 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48095783?s=460&v=4"
+            alt="Julio Cesar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJs & React Native forms! 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/48095783?s=460&v=4"
+            alt="Julio Cesar"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJs & React Native forms! 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   )
 }
 
