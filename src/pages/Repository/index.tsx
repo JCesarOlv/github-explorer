d83d79b1f9e75e4385ec230fb17bb226f371b59d
Julio Cesar <jcesar_olv@outlook.com>
2020-12-09 17:07:03 -0300

    stylizing the details

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
 
 interface RepositoryParams {
   repository: string;
@@ -9,7 +15,50 @@ const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
   return (
-    <h1>Repository: {params.repository}</h1>
+    <>
+      <Header >
+        <img src={logoImg} alt="Github Explorer"/>
+        <Link to='/'>
+          <FiChevronLeft  size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars.githubusercontent.com/u/28929274?v=4" alt="Rocketseat"/>
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descriçao do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1888</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to=''>
+          <div>
+            <strong></strong>
+            <p></p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
   )
 }
 
